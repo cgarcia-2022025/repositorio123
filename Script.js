c4acb02dd418b57ab883b7e7e76f32495a182d13
cgarcia-2022025 <134984794+cgarcia-2022025@users.noreply.github.com>
2024-01-19 09:11:33 -0600

    Se actualizo el archivo a las 9:00

--- Script.js
+++ Script.js
@@ -1,12 +1,25 @@
-const palabras = ["ELEFANTE", "GIRASOL", "COMPUTADORA", "TELEVISOR", "MURCIELAGO", "ESCALERA", "PIZARRA", "AVION", "TELEFONO", "COCODRILO", "ZEBRA", "GUITARRA", "AUTOMOVIL", "CABALLO", "BICICLETA"];
-let palabraSecreta = "";
-let palabraOculta = [];
-let letrasUtilizadas = [];
 
-
-var a = document.getElementById("myCanvas");
+function area(){
+var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
-ctx.arc(85, 45, 35, 0, 2 * Math.PI);
+ctx.arc(100, 68, 25, 0, 2 * Math.PI);
 ctx.stroke();
-
+var a = document.getElementById("myCanvas");
+var ctx = a.getContext("2d");
+ctx.beginPath()
+ctx.lineWidth=4
+ctx.moveTo(25, 25)
+ctx.lineTo(100,25)
+ctx.lineTo(100,40)
+ctx.moveTo(25,25)
+ctx.lineTo(25,150)
+ctx.stroke()
+var b = document.getElementById("myCanvas");
+ctx.beginPath()
+ctx.lineWidth=2
+ctx.moveTo(100,95)
+ctx.lineCap='round'
+ctx.lineTo(100,115)
+ctx.stroke()
+}
