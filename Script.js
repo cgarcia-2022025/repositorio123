d58dfd72591c95c626761a288577bc51060d2edb
cgarcia-2022025 <134984794+cgarcia-2022025@users.noreply.github.com>
2024-01-19 08:40:51 -0600

    se Modifico el archivo javaScript a las 8:30

--- Script.js
+++ Script.js
@@ -1,6 +1,12 @@
+const palabras = ["ELEFANTE", "GIRASOL", "COMPUTADORA", "TELEVISOR", "MURCIELAGO", "ESCALERA", "PIZARRA", "AVION", "TELEFONO", "COCODRILO", "ZEBRA", "GUITARRA", "AUTOMOVIL", "CABALLO", "BICICLETA"];
+let palabraSecreta = "";
+let palabraOculta = [];
+let letrasUtilizadas = [];
+
 
 var a = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(85, 45, 35, 0, 2 * Math.PI);
 ctx.stroke();
+
